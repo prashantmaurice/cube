51d8a22070c7f3c03a23146643d6d5bd74988ce7
Prashant Maurice <sabertoothmaurice@gmail.com>
2015-11-06 02:24:32 +0530

    Showing Grid on top of image

--- app/src/main/java/com/maurice/app/cube/ImageParser/ImageParser.java
+++ app/src/main/java/com/maurice/app/cube/ImageParser/ImageParser.java
@@ -554,33 +554,14 @@ public class ImageParser {
 
         Logg.d("CONTOUR", "screen size2 : " + screenContourRect.rows());
 
-        //=====================
+        //Find Camera Extrinsic and Intrinsic variables
         if(image==null){
             Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.image);
             image = GenUtils.convertBitmapToMat(bitmap);
         }
-        final Size mPatternSize = new Size(4, 11);
-        final int mCornersSize = (int)(mPatternSize.width * mPatternSize.height);
-        boolean mPatternWasFound = false;
-        MatOfPoint2f mCorners = new MatOfPoint2f();
-        List<Mat> mCornersBuffer = new ArrayList<Mat>();
-        boolean mIsCalibrated = false;
-
-        Mat mCameraMatrix = new Mat();
         MatOfDouble mDistortionCoefficients = new MatOfDouble();
-        int mFlags;
-        double mRms;
-        double mSquareSize = 0.0181;
-        Size mImageSize;
-        ArrayList<Mat> rvecs = new ArrayList<Mat>();
-        ArrayList<Mat> tvecs = new ArrayList<Mat>();
-        Mat reprojectionErrors = new Mat();
-        ArrayList<Mat> objectPoints = new ArrayList<Mat>();
-        objectPoints.add(Mat.zeros(mCornersSize, 1, CvType.CV_32FC3));
-//        calcBoardCornerPositions(objectPoints.get(0));
-        for (int i = 1; i < mCornersBuffer.size(); i++) {
-            objectPoints.add(objectPoints.get(0));
-        }
+        ArrayList<Mat> rvecs = new ArrayList<>();
+        ArrayList<Mat> tvecs = new ArrayList<>();
 
         Mat intrinsic = new Mat(3, 3, CvType.CV_32FC1);
         double f = 100*CameraActivity.seek;//focal length of camera
@@ -608,10 +589,6 @@ public class ImageParser {
 
         ArrayList<Mat> points3D = new ArrayList<>();
         Mat a1 = new MatOfPoint3f();
-//        a1.push_back(new MatOfPoint3f(new Point3(rect.lt.x, rect.lt.y, 0)));
-//        a1.push_back(new MatOfPoint3f(new Point3(rect.rt.x, rect.rt.y, 0)));
-//        a1.push_back(new MatOfPoint3f(new Point3(rect.rb.x, rect.rb.y, 0)));
-//        a1.push_back(new MatOfPoint3f(new Point3(rect.lb.x, rect.lb.y, 0)));
         a1.push_back(new MatOfPoint3f(new Point3(w, 0, 0)));
         a1.push_back(new MatOfPoint3f(new Point3(0, 0, 0)));
         a1.push_back(new MatOfPoint3f(new Point3(0, h, 0)));
@@ -628,138 +605,105 @@ public class ImageParser {
 
         //calibrateCamera(objectPoints, imagePoints, imageSize, cameraMatrix, distCoeffs, rvecs, tvecs, flags)
         Calib3d.calibrateCamera(points3D, points2D, new Size(w,h), intrinsic, mDistortionCoefficients, rvecs, tvecs);
-        Logg.d("OUTPUT4", "" + Arrays.toString(tvecs.get(0).get(0, 0)));
+//        Logg.d("OUTPUT4", "" + Arrays.toString(tvecs.get(0).get(0, 0)));
         rect.print();
 
+
+        //Check if those values are correct
+//        MatOfPoint3f a3 = new MatOfPoint3f();
+//        a3.push_back(new MatOfPoint3f(new Point3(w, 0, 0)));
+//        a3.push_back(new MatOfPoint3f(new Point3(0, 0, 0)));
+//        a3.push_back(new MatOfPoint3f(new Point3(0, h, 0)));
+//        a3.push_back(new MatOfPoint3f(new Point3(w, h, 0)));
+//        MatOfPoint2f a4 = new MatOfPoint2f();
+//
+//
+//        Mat jacobian = new Mat();//optional output
+//        double aspectRatio = 0;//optional output
+//        Calib3d.projectPoints(a3, rvecs.get(0), tvecs.get(0), intrinsic, mDistortionCoefficients, a4, jacobian, aspectRatio);
+//
+//        Logg.d("OUTPUT0", "" + rect.lt.toString());
+//        Logg.d("OUTPUT1", "" + Arrays.toString(a2.get(0,0)));
+//        Logg.d("OUTPUT2", ""+Arrays.toString(a4.toArray()));
+//        Rectangle outputRect = new Rectangle(new MatOfPoint(a4.toArray()));
+
+
+
+        //Remove white color from screen
+        List<MatOfPoint> list = new ArrayList<>();
+        list.add(screenContour);
+        Imgproc.fillPoly(srcGryColor, list, new Scalar(0,0,0));
+
+        //Create mapper that will be displayed inside screen
+        Mat onlyGrid = new Mat(srcGryColor.size(),srcGryColor.type());
+        Mat srcOverlayWhite = new Mat(srcGryColor.size(),srcGryColor.type());
+        Mat srcOverlayActual = new Mat(srcGryColor.size(),srcGryColor.type());
+        List<MatOfPoint> listTemp2 = new ArrayList<>();
+        listTemp2.add(screenContour);
+        Imgproc.fillPoly(srcOverlayWhite, listTemp2, new Scalar(255, 255, 255));
+        int gridLength = 30;
+        int gridNum = 10;
+        for(int p=0;p<gridNum;p++){
+
+            List<Point3> points = new ArrayList<>();
+            points.add(new Point3(w, 0, -p*gridLength));
+            points.add(new Point3(0, 0, -p*gridLength));
+            points.add(new Point3(0, h, -p*gridLength));
+            points.add(new Point3(w, h, -p*gridLength));
+            Rectangle outputRect = findCameraPerspectiveOf(points,rvecs.get(0), tvecs.get(0), intrinsic, mDistortionCoefficients);
+
+            //Draw using resulting
+            int colorShade = 255-(p*255/gridNum);
+            Scalar colorScalar = new Scalar(colorShade,colorShade,colorShade);
+            for(int i=0;i<4;i++){
+                Imgproc.line(onlyGrid, outputRect.lb, outputRect.lt, colorScalar, 1);
+                Imgproc.line(onlyGrid, outputRect.lb, outputRect.rb, colorScalar, 1);
+                Imgproc.line(onlyGrid, outputRect.rb, outputRect.rt, colorScalar, 1);
+                Imgproc.line(onlyGrid, outputRect.rt, outputRect.lt, colorScalar, 1);
+            }
+
+            //Add in mapper
+            Mat temp2 = new Mat(srcGryColor.size(),srcGryColor.type());
+            Core.bitwise_and(srcOverlayWhite, onlyGrid, temp2);
+            Core.add(srcOverlayActual, temp2, srcOverlayActual);
+        }
+
+
+
+
+//        srcOverlayActual
+//        Core.bitwise_and(roi, roi,srcGry,src4);
+
+
+
+
+        //overlay mapper on top of original image
+        Core.add(srcGryColor, srcOverlayActual, srcGryColor);
+
+
+
+
+
+        return srcGryColor;
+    }
+
+    private Rectangle findCameraPerspectiveOf(List<Point3> points, Mat rvec, Mat tvec, Mat intrinsic, MatOfDouble mDistortionCoefficients) {
+        //Check if those values are correct
         MatOfPoint3f a3 = new MatOfPoint3f();
-        a3.push_back(new MatOfPoint3f(new Point3(w, 0, 0)));
-        a3.push_back(new MatOfPoint3f(new Point3(0, 0, 0)));
-        a3.push_back(new MatOfPoint3f(new Point3(0, h, 0)));
-        a3.push_back(new MatOfPoint3f(new Point3(w, h, 0)));
+        a3.push_back(new MatOfPoint3f(points.get(0)));
+        a3.push_back(new MatOfPoint3f(points.get(1)));
+        a3.push_back(new MatOfPoint3f(points.get(2)));
+        a3.push_back(new MatOfPoint3f(points.get(3)));
         MatOfPoint2f a4 = new MatOfPoint2f();
 
-        //projectPoints(objectPoints, rvec, tvec, cameraMatrix, distCoeffs, imagePoints, jacobian, aspectRatio)
+
         Mat jacobian = new Mat();//optional output
         double aspectRatio = 0;//optional output
-        //projectPoints(MatOfPoint3f objectPoints, Mat rvec, Mat tvec, Mat cameraMatrix, MatOfDouble distCoeffs, MatOfPoint2f imagePoints, Mat jacobian, double aspectRatio)
+        Calib3d.projectPoints(a3, rvec, tvec, intrinsic, mDistortionCoefficients, a4, jacobian, aspectRatio);
 
-        Calib3d.projectPoints(a3, rvecs.get(0), tvecs.get(0), intrinsic, mDistortionCoefficients, a4, jacobian, aspectRatio);
-        Logg.d("OUTPUT0", "" + rect.lt.toString());
-        Logg.d("OUTPUT1", "" + Arrays.toString(a2.get(0,0)));
-        Logg.d("OUTPUT2", ""+Arrays.toString(a4.toArray()));
-
-        ;
-        Rectangle outputRect = new Rectangle(new MatOfPoint(a4.toArray()));
-
-//        Core.gemm(intrinsic, cameraPosition, 1, new Mat(), 0, vec4t);
-//        Core.gemm(vec4t, point, 1, new Mat(), 0, result);
-
-        //==============
-
-
-
-
-
-
-        //Find homography
-//        if(image==null){
-//            Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.image);
-//            image = GenUtils.convertBitmapToMat(bitmap);
-//        }
-//        Point[] imagePoints = new Point[4];
-//        int w = image.width();
-//        int h = image.height();
-//        imagePoints[0] = new Point(w, 0);
-//        imagePoints[1] = new Point(0, 0);
-//        imagePoints[2] = new Point(0, h);
-//        imagePoints[3] = new Point(w, h);
-//        MatOfPoint2f imagePoints2F = new MatOfPoint2f();
-//        imagePoints2F.fromArray(imagePoints);
-//
-//
-//        Mat homography = org.opencv.imgproc.Imgproc.getPerspectiveTransform(screenContourRect2F, imagePoints2F);
-//        Mat intrinsicInverse = new Mat(3, 3, CvType.CV_32FC1);
-//
-//        //Form camera intrinsic matrix
-//        //https://dsp.stackexchange.com/questions/2736/step-by-step-camera-pose-estimation-for-visual-tracking-and-planar-markers
-//        Mat intrinsic = new Mat(3, 3, CvType.CV_32FC1);
-//        double f = 35;//focal length of camera
-//        double k = 10;//pixels per unit distance
-//        double a = k*f; //scale factor
-//        double s = 0;//s is the skew, only non-zero if u and v are non-perpendicular.
-//        double u = image.width()/2;
-//        double v = image.height()/2;
-//        intrinsic.put(0,0,a);
-//        intrinsic.put(0,1,s);
-//        intrinsic.put(0,2,u);
-//        intrinsic.put(1,0,0);
-//        intrinsic.put(1,1,a);
-//        intrinsic.put(1,2,v);
-//        intrinsic.put(2,0,0);
-//        intrinsic.put(2,1,0);
-//        intrinsic.put(2,2,1);
-//
-//
-//
-//        Core.invert(intrinsic, intrinsicInverse);
-//        intrinsicInverse.convertTo(intrinsicInverse, CvType.CV_32FC1);
-//        homography.convertTo(homography, CvType.CV_32FC1);
-//// compute H respect the intrinsics
-//        Core.gemm(intrinsicInverse, homography, 1, new Mat(), 0, homography);
-//
-//        Mat pose = cameraPoseFromHomography(homography);
-//
-//        Mat origin3D = new Mat(3, 1, CvType.CV_32FC1);
-//        origin3D.put(0,0,0);
-//        origin3D.put(1,0,0);
-//        origin3D.put(2, 0, 0);
-//        Mat origin2D = intrinsic.mul(pose).mul(origin3D);
-//        Logg.d("origin2D",origin2D.toString());
-
-
-
-
-//        Mat H = Calib3d.findHomography(screenContourRect2F, imagePoints2F);
-
-
-//        Mat srcGry2 = src.clone();
-//        Mat srcGryColor2 = src.clone();//used for overlaying stuff
-//        Imgproc.cvtColor(src, srcGry2, Imgproc.COLOR_RGB2GRAY);
-//        Imgproc.cvtColor(srcGry2, srcGryColor2, Imgproc.COLOR_GRAY2BGR);
-//        Mat srcGry3 = image.clone();
-//        Mat srcGryColor3 = image.clone();//used for overlaying stuff
-//        Imgproc.cvtColor(image, srcGry3, Imgproc.COLOR_RGB2GRAY);
-//        Imgproc.cvtColor(srcGry3, srcGryColor3, Imgproc.COLOR_GRAY2BGR);
-//        Mat color2 = addImageOnExisting(srcGryColor2, rect, image);
-//        Mat color2 = addImageOnExisting(srcGryColor2, outputRect, image);
-
-
-//        Mat color5 = new Mat();
-//        Imgproc.cvtColor(srcGry, color5, Imgproc.COLOR_GRAY2BGR);
-        Scalar colorScalar = new Scalar(0,200,100);
-        for(int i=0;i<4;i++){
-            Imgproc.line(srcGryColor, outputRect.lb, outputRect.lt, colorScalar, 2);
-            Imgproc.line(srcGryColor, outputRect.lb, outputRect.rb, colorScalar, 2);
-            Imgproc.line(srcGryColor, outputRect.rb, outputRect.rt, colorScalar, 2);
-            Imgproc.line(srcGryColor, outputRect.rt, outputRect.lt, colorScalar, 2);
-        }
-//        MainActivity.setDebugImage(color5, 5);
-        if(true) return srcGryColor;
-
-
-//        if(true) return color2;
-//        addImageOnExisting()
-
-
-        //Main
-//        Imgproc.drawContours(srcGryColor, contoursList, 0, new Scalar(0,125,0));
-
-        if(true) return srcGryColor;
-
-
-
-        return null;
+        return new Rectangle(new MatOfPoint(a4.toArray()));
     }
+
 
     private Mat cameraPoseFromHomography(Mat h) {
         Log.d("DEBUG", "cameraPoseFromHomography: homography : " + h.toString());
